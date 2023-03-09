31d58deb8a19a0143b69724d51de9a195ce6df11
jasoncabusao <jasoncabusao@Jasons-MacBook-Pro-15.local>
2023-03-08 16:57:53 -0800

    fixed labeling

--- server.R
+++ server.R
@@ -85,8 +85,7 @@ my_server <- function(input,output){
                                          fill = factor(depression_severity, 
                                                        levels = c("none", "None-minimal", "Mild", "Moderate", 
                                                                   "Moderately severe", "Severe")),
-                                         text = paste("Depression Severity:", depression_severity, "<br>",
-                                                      "Percent of Students:"))) +
+                                         text = paste("Depression Severity:", depression_severity, "<br>"))) +
         geom_bar(stat = "count",
                  position = "fill") +
         scale_fill_brewer(palette = "Oranges") +
@@ -102,7 +101,7 @@ my_server <- function(input,output){
   
   # Chart 3 analysis
     output$chart3_analysis <- renderText({
-    "This chart is a data visualization that shows the correlation between class standing and depression severity in college students. We wanted to see if certain college years made a students depression more severe. Since coursework tends to get harder the further you are into college, we wanted to determine if the severity of depression gets worse too? From the graph, it shows that the higher year of college doesn't increase a students depression severity. There are actually more first year college students with severe depression than fourth year college students with severe depression. With the selection choices on the left-hand side, users are able to choose which years they want to compare."
+    "This chart is a data visualization that shows the correlation between class standing and depression severity in college students. We wanted to see if certain college years made a students depression more severe. Since coursework tends to get harder the further you are into college, we wanted to determine if the severity of depression gets worse too? When you select all 4 college years on the graph, it shows that the higher year of college doesn't increase a students depression severity. There are actually more first year college students with severe depression than fourth year college students with severe depression. With the selection choices on the left-hand side, users are able to choose which years they want to compare. Users can compare the percentage of certain depression severity's for different college years. Additionally, users can isolate certain depression severity's by clicking on it on the right hand side of the graph as they compare different college years."
     })
       
 }
